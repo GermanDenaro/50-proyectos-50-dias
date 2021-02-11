a7b84ffca720d092887bdfc3caef0b01fc452eb8
German] <germandenaroc@gmail.com>
2021-02-11 15:55:34 -0300

    FAQ Collapse

--- 10- Dad Jokes/main.js
+++ 10- Dad Jokes/main.js
@@ -7,30 +7,30 @@ jokeBtn.addEventListener('click', generateJoke)
 generateJoke();
 
     // USING ASYNC/AWAIT
-async function generateJoke() {
-  const config = {
-    headers: {
-      Accept: 'application/json',
-    },
-  }
+// async function generateJoke() {
+//   const config = {
+//     headers: {
+//       Accept: 'application/json',
+//     },
+//   }
 
-  const res = await fetch('https://icanhazdadjoke.com', config)
+//   const res = await fetch('https://icanhazdadjoke.com', config)
 
-  const data = await res.json()
+//   const data = await res.json()
 
-  jokeEl.innerHTML = data.joke
-}
-
-// function generateJoke() {
-//     const config = {
-//         headers: {
-//             'Accept': 'application/json'
-//         }
-//     }
-
-//     fetch('https://icanhazdadjoke.com', config)
-//         .then((res) => res.json())
-//         .then(data => {
-//             jokeEl.innerHTML = data.joke
-//         })
+//   jokeEl.innerHTML = data.joke
 // }
+
+ function generateJoke() {
+     const config = {
+         headers: {
+             'Accept': 'application/json'
+         }
+     }
+
+     fetch('https:icanhazdadjoke.com', config)
+         .then(res => res.json())
+         .then(data => {
+             jokeEl.innerHTML = data.joke
+         })
+ }
